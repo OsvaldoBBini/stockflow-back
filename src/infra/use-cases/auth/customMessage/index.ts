2eaf9f6926ab98aabac495d10cfe483c128ce257
Osvaldo <osvaldobbini@gmail.com>
2026-04-12 14:49:48 -0300

    fix: forgot message

--- src/infra/use-cases/auth/customMessage/index.ts
+++ src/infra/use-cases/auth/customMessage/index.ts
@@ -6,7 +6,7 @@ export async function handler(event: CustomMessageTriggerEvent) {
 
   const code = event.request.codeParameter;
   const name = event.request.userAttributes.given_name;
-  const email = event.request.userAttributes.email;
+  // const email = event.request.userAttributes.email;
  
   if (event.triggerSource === 'CustomMessage_SignUp') {
     const { subject, emailMessage } = singUpMessage(name, code);
@@ -15,7 +15,7 @@ export async function handler(event: CustomMessageTriggerEvent) {
   }
 
   if (event.triggerSource === 'CustomMessage_ForgotPassword') {
-    const { subject, emailMessage } = forgotPasswordMessage(email, code);
+    const { subject, emailMessage } = forgotPasswordMessage(code);
     event.response.emailSubject = subject;
     event.response.emailMessage = emailMessage;
   }
