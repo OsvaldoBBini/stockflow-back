93feb1331a7f21116df5fcdf5112ad99e787e0a6
Osvaldo <osvaldobbini@gmail.com>
2026-04-12 14:29:07 -0300

    style: add logger

--- src/infra/use-cases/profile/me/me.ts
+++ src/infra/use-cases/profile/me/me.ts
@@ -13,20 +13,24 @@ export async function handler(event: APIGatewayProxyEventV2) {
     const cognitoClient = new CognitoIdentityProviderClient();
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const userId = (event.requestContext as any).authorizer.jwt.claims.sub;
-  
+
+    logger.debug({ message: 'Get profile for user', userId });
+    
     const command = new AdminGetUserCommand({
       Username: userId,
       UserPoolId: process.env.COGNITO_POOL_ID
     });
-  
+    
     const { UserAttributes } = await cognitoClient.send(command);
-
+    
     const attributes = UserAttributes && UserAttributes.reduce((acc, post) => {
       const { Name, Value } = post;
       if (!Name || !Value) return acc;
       return {...acc, [Name]: Value};
     }, {});
-  
+
+    logger.debug({ message: 'Get attributes', attributes });
+    
     return {
       statusCode: 200,
       body: JSON.stringify(attributes),
